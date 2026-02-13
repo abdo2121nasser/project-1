fd50bac16a0c743e8b94d60b2a7452949570d7ba
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2026-02-13 18:13:07 +0200

    init project

---
+++ lib/app_root/app_root.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class SpecialistBookerApp extends StatelessWidget {
+  const SpecialistBookerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: MainShellScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:p_134/app_root/app_root.dart';
+
+void main() {
+
+  runApp(SpecialistBookerApp());
+}
